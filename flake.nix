357c09e56efee4662cab28c3d5fc023df056eba8
zmrocze <karolochmanmilarski@gmail.com>
2024-04-23 23:16:21 +0200

    Try building with nixpkgs 23.11, upstream agda

---
+++ flake.nix
@@ -0,0 +1,37 @@
+
+{
+  description = "A very basic flake";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    haskellNix.inputs.nixpkgs-unstable.follows = "nixpkgs";
+    # nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOs/nixpkgs/23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    # url = "git+file:/home/zmrocze/code/packages/agda-language-server?ref=c2ae939";
+    agda-language-server = {
+      url = "github:agda/agda-language-server";
+      flake = false;
+    };
+  };
+
+  outputs = { self, agda-language-server, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          helloProject =
+            final.haskell-nix.project' {
+              src = agda-language-server;
+              compiler-nix-name = "ghc928";
+              shell.tools = {};
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.helloProject.flake {};
+    in flake // {
+      # Built by `nix build .`
+      packages.default = flake.packages."agda-language-server:exe:als";
+    });
+}
